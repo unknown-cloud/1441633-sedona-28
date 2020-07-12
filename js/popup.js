a54dd0508177d4b9e4a7855b9257fc79aa9ea511
unknown-cloud <63306171+unknown-cloud@users.noreply.github.com>
2020-07-12 19:26:50 +0700

    Добавила анимацию и обязательное заполнение полей

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,47 @@
 var link = document.querySelector(".link-search-hotel");
 var popup = document.querySelector(".popup");
+var form = popup.querySelector(".visit-form");
+var arrival = popup.querySelector(".arrival");
+var departure = popup.querySelector(".departure");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("arrival");
+} catch (err) {
+  isStorageSupport = false;
+}
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.toggle("modal-show");
+
+  if (storage) {
+    arrival.value = storage;
+    departure.focus();
+  } else {
+    arrival.focus();
+  }
 });
+
+form.addEventListener("submit", function (evt) {
+  if (!arrival.value || !departure.value) {
+      evt.preventDefault();
+      console.log("Введите данные");
+      popup.classList.add("modal-error");
+    } else {
+      if (isStorageSupport) {
+        localStorage.setItem("arrival", arrival.value);
+  }
+}
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if(popup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      popup.classList.remove("modal-show");
+    }
+  }
+})
